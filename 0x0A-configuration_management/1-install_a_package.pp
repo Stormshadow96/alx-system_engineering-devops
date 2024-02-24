1099fd918384d1a5ebc40544a4e31f544f002764
Stormshadow96 <jletsoalo257@gmail.com>
2024-02-23 16:43:06 -0800

    whole task redo

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
-# Install a specific version of flask (2.1.0)
-
-package {'flask':
+# A manifest to istall flask v2.1.0 on puppet-agent
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider =>  pip3,
 }
 
