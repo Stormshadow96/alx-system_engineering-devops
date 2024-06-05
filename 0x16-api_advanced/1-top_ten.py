0b9338400ddff7567a768e6cb81318fcd9015862
stormshadow96 <jletsoalo257@gmail.com>
2024-06-05 23:38:28 +0000

    api-advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,17 +7,14 @@ import requests
 
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot/.json?show="all"&limit=10".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    response = requests.get(url, headers=headers, allow_redirects=False)
+	try:
+	   top_ten = response.json()['data']['childern']
+	   for post in top_ten:
+		print(post['data']['title'])
+	except KeyError:
+	    print("None")
